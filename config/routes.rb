d2392dc88bbe7aa889709d14f6674d1970297dea
hoapng <giahoa01giathieu@gmail.com>
2023-01-30 15:12:10 +0000

    add image on book

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :books
   get 'static_pages/home'
   devise_for :users
   resources :books
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_12_100059) do
+ActiveRecord::Schema.define(version: 2023_01_30_150516) do
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.integer "number_of_pages"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
